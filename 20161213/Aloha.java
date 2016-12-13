e0140c3f06860c7a6dc97748517568c5c0215d3a
DAKSHAL <160153@yse-c.net>
2016-12-13 10:11:26 +0900

    Aloha.java4作りました

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,7 +1,12 @@
 public class Aloha {
 	public static void main(String[] args) {
-		for(int i=0;i<20;i++){
-				System.out.println(args[0]);
+		while(true){
+			System.out.print("Input data >");
+			String data = new java.util.Scanner(System.in).nextLine();
+			System.out.println(data);
+
+			if( data.equals("exit") ){
+				break;
 			}
 		}
 	
